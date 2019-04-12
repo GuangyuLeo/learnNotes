316d9592ead893252325937fefd9bcc11188fbcb
LiaoYanxi <liaoyanxi@hotmail.com>
2019-04-12 18:06:31 +0800

    新增GITT别名配置文件

---
+++ Git别名配置.sh
@@ -0,0 +1,8 @@
+#bash
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.unstage 'reset HEAD'
+git config --global alias.last 'log -l'
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
